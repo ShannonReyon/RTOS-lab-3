64d467a055b01a1a0e18b55b76e20b1fe72a693d
Shannon_Reyon <68496784+ShannonReyon@users.noreply.github.com>
2022-04-28 02:49:27 +0530

    part3 done

--- part3_led_blink/part3_led_blink.ino
+++ part3_led_blink/part3_led_blink.ino
@@ -1,29 +1,26 @@
-//Configure output pins
+
 static const int led_pin = LED_BUILTIN;
 
-//TASK OF BLINK
 void BlinkLED(void * parameter){
     while(1){
         digitalWrite(led_pin,HIGH);
-        vTaskDelay(500/portTICK_PERIOD_MS); //portTick_PERIOD_MS = 1ms
+        vTaskDelay(500/portTICK_PERIOD_MS); 
         digitalWrite(led_pin,LOW);
         vTaskDelay(500/portTICK_PERIOD_MS);
       }
   }
 
   void setup(){
-      //configuration pin
+     
       pinMode(led_pin,OUTPUT);
-
-      //Task to run forever
       xTaskCreatePinnedToCore(
-          BlinkLED, //fn to be called
-          "Blink LED", //Name of task
-          1024, //stack size(bytes in esp32,words in FreeRTOS,,,,,,min b = 768
-          NULL, //parameter to pass to function
-          1,  //Task priority (0 to configMAX_PRIORITIES -1)
-          NULL, //Task handle
-          1);   //run on one core
+          BlinkLED, 
+          "Blink LED", 
+          1024, 
+          NULL, 
+          1,  
+          NULL, 
+          1);  
     }
 
     void loop(){
