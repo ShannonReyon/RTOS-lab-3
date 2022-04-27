ccffe90c0727711ae7e014f0b162cdcd391aac71
Shannon_Reyon <68496784+ShannonReyon@users.noreply.github.com>
2022-04-28 04:39:07 +0530

    part 3.3 done

---
+++ part3_led_blink_3/part3_led_blink_3.ino
@@ -0,0 +1,48 @@
+static const int rate_1 = 500;  
+static const int rate_2 = 300;  
+
+static const int led_pin = 32;
+
+// Task 1
+void LED_1(void *parameter) {
+  while(1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(rate_1 / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(rate_1 / portTICK_PERIOD_MS);
+  }
+}
+
+// Task 2
+void LED_2(void *parameter) {
+  while(1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(rate_2 / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(rate_2 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+
+ xTaskCreatePinnedToCore(  
+              LED_1,  
+              "LED 1",   
+              1024,         
+              NULL,        
+              1,           
+              NULL,         
+              1);     
+
+  xTaskCreatePinnedToCore(  
+              LED_2,  
+              "LED 2",   
+              1024,         
+              NULL,        
+              1,           
+              NULL,         
+              1);  
+}
+
+void loop() {
+}
