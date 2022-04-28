3ae60bf67b43932c5aa3ee9379949f5b25949f94
Shannon Reyon <68496784+ShannonReyon@users.noreply.github.com>
2022-04-29 04:31:23 +0530

    Add files via upload

---
+++ part6_mutex/part6_mutex.ino
@@ -0,0 +1,70 @@
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+// Globals
+static int shared_var = 0;
+static SemaphoreHandle_t mutex;
+
+void incTask(void *parameters) {
+
+  int local_var;
+
+  while (1) {
+
+    if (xSemaphoreTake(mutex, 0) == pdTRUE) {
+
+      local_var = shared_var;
+      local_var++;
+      vTaskDelay(random(100, 500) / portTICK_PERIOD_MS);
+      shared_var = local_var;
+     
+      Serial.println(shared_var);
+ 
+      xSemaphoreGive(mutex);
+
+    } else {
+      
+    }
+  }
+}
+
+
+void setup() {
+
+  randomSeed(analogRead(0));
+
+  Serial.begin(115200);
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Race Condition Demo---");
+
+  mutex = xSemaphoreCreateMutex();
+
+  xTaskCreatePinnedToCore(incTask,
+                          "Increment Task 1",
+                          1024,
+                          NULL,
+                          1,
+                          NULL,
+                          app_cpu);
+
+  xTaskCreatePinnedToCore(incTask,
+                          "Increment Task 2",
+                          1024,
+                          NULL,
+                          1,
+                          NULL,
+                          app_cpu);           
+
+  // Delete "setup and loop" task
+  vTaskDelete(NULL);
+}
+
+void loop() {
+  // Execution should never get here
+}
+
